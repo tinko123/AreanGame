78fcd352a6ec59191aa581eec45b80fc5b2229a4
tink0t0 <tinkogroshev@abv.bg>
2024-05-22 11:18:53 +0300

    adding new weapon mace

---
+++ ArenaGame/Weapons/Mace.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Weapons
+{
+    public class Mace : IWeapon
+    {
+        public string Name { get; set; }
+
+        public double AttackDamage { get; private set; }
+
+        public double BlockingPower { get; private set; }
+        Random random = new Random();
+        public Mace(string name)
+        {
+            double coef = random.NextDouble();
+            Name = name;
+            if (coef <= 0.1)
+            {
+                AttackDamage = 100;
+            }
+            else
+            {
+                AttackDamage = 20;
+            }
+            BlockingPower = 10;
+        }
+    }
+}

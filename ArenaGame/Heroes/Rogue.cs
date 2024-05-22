b3e84460cb9a0b483929714a0bac384e41a77be1
tink0t0 <tinkogroshev@abv.bg>
2024-05-22 12:02:58 +0300

    creating hero rogue

---
+++ ArenaGame/Heroes/Rogue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Rogue :Hero
+    {
+        public Rogue(string name, double armor, double strenght, IWeapon weapon) : base(name, armor, strenght, weapon)
+        {
+        }
+
+        public override double Attack()
+        {
+            return base.Attack();
+        }
+
+        public override double Defend(double damage)
+        {
+            return base.Defend(damage);
+        }
+    }
+    {
+    }
+}

8b8a96d8a6592cc4d8e36a864a8e7045b06a13d2
tink0t0 <tinkogroshev@abv.bg>
2024-05-22 11:56:32 +0300

    creating hero mage

---
+++ ArenaGame/Heroes/Mage.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArenaGame.Heroes
+{
+    public class Mage : Hero
+    {
+        public Mage(string name, double armor, double strenght, IWeapon weapon) : base(name, armor, strenght, weapon)
+        {
+        }
+
+        public override double Attack()
+        {
+            return base.Attack();
+        }
+
+        public override double Defend(double damage)
+        {
+            return base.Defend(damage);
+        }
+    }
+}
